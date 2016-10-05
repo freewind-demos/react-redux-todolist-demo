5d9c9474df21ca9f4fc1af0364295afd6503a434
Peng.Li <nowind_lee@qq.com>
2016-10-05 13:23:06 +0800

    use `should` style

--- spec/components/hello-spec.js
+++ spec/components/hello-spec.js
@@ -2,24 +2,25 @@
 
 import React from 'react';
 import Hello from '../../src/components/Hello.jsx';
-import chai, {expect}  from 'chai';
+import chai from 'chai';
 import spies  from 'chai-spies';
 import {shallow} from 'enzyme';
 
+chai.should();
 chai.use(spies);
 
 describe('<Hello />', () => {
 
   it('shows fruit names', () => {
     const wrapper = shallow(<Hello fruit={['AAA', 'BBB']}/>);
-    expect(wrapper.contains('AAA')).to.equal(true);
-    expect(wrapper.contains('BBB')).to.equal(true);
+    wrapper.contains('AAA').should.be.true;
+    wrapper.contains('BBB').should.be.true;
   });
 
   it('calls onFruitDelete if deletes a fruit', () => {
     const spy = chai.spy();
     const wrapper = shallow(<Hello fruit={['AAA', 'BBB']} onDeleteFruit={spy}/>);
     wrapper.find('button[title="delete"]').first().simulate('click');
-    expect(spy).to.have.been.called.with('AAA');
+    spy.should.have.been.called.with('AAA');
   })
 });
